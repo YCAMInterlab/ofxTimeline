67f1e18831c97eecaccd603e6f8b279d9fa65448
obviousjim <james@jamesgeorge.org>
2011-10-02 17:25:02 +0200

    removed la la la
    Signed-off-by: obviousjim <james@jamesgeorge.org>

--- src/ofxTLElementHeader.cpp
+++ src/ofxTLElementHeader.cpp
@@ -32,7 +32,7 @@ void ofxTLElementHeader::draw(){
 	ofSetColor(250, 180, 80);
 	
 	ofNoFill();
-	ofDrawBitmapString( name + " header la la la ", ofPoint(bounds.x + 30, bounds.y + 15) );
+	ofDrawBitmapString( name, ofPoint(bounds.x + 30, bounds.y + 15) );
 	ofRect(bounds);
 	
 	ofPopStyle();
