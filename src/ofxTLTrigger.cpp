35c432cf193d0cddcbaf139bde7a93921b7b9a65
obviousjim <james@jamesgeorge.org>
2011-12-04 23:58:15 -0500

    fixed backspace delete trigger bug when editing trigger text
    Signed-off-by: obviousjim <james@jamesgeorge.org>

--- src/ofxTLTrigger.cpp
+++ src/ofxTLTrigger.cpp
@@ -167,7 +167,8 @@ void ofxTLTrigger::mouseReleased(ofMouseEventArgs& args){
 
 void ofxTLTrigger::keyPressed(ofKeyEventArgs& args){
 	
-	if((args.key == OF_KEY_DEL || args.key == OF_KEY_BACKSPACE) && selectedTrigger != NULL){
+	
+	if((args.key == OF_KEY_DEL || args.key == OF_KEY_BACKSPACE) && selectedTrigger != NULL && !enterText){
 		for(int i = triggers.size()-1; i>=0; i--){
 			if(&triggers[i] == selectedTrigger){
 				triggers.erase(triggers.begin()+i);
