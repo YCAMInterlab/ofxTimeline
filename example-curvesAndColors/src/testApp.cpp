33919de3b86e9aacfea4db74a4fef921a8223d4e
James George <james@jamesgeorge.org>
2013-01-08 11:45:29 -0500

    fixed curves and colors example

--- example-curvesAndColors/src/testApp.cpp
+++ example-curvesAndColors/src/testApp.cpp
@@ -1,8 +1,32 @@
 /**
- * Curves demo
- * ofxTimeline
+ * ofxTimeline -- AudioWaveform Example
+ * openFrameworks graphical timeline addon
  *
- * Shows how to use ofxTimeline to create a simple curves and color change
+ * Copyright (c) 2011-2012 James George http://www.jamesgeorge.org
+ * Development Supported by YCAM InterLab http://interlab.ycam.jp/en/ +
+ *
+ * http://github.com/YCAMinterLab
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #include "testApp.h"
@@ -21,6 +45,8 @@ void testApp::setup(){
 	light.enable();
 		
     //Timeline setup and playback details
+    ofxTimeline::removeCocoaMenusFromGlut("CurvesColorsDemo");
+    
 	timeline.setup();
     timeline.setFrameRate(30);
 	timeline.setDurationInFrames(90);
