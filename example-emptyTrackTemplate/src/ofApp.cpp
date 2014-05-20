8578e40a67803be51ac3da9cf6662c3927649887
James George <james@jamesgeorge.org>
2014-05-20 11:47:39 -0400

    updated empty example for 0.8.1 and VS 2012

--- example-emptyTrackTemplate/src/ofApp.cpp
+++ example-emptyTrackTemplate/src/ofApp.cpp
@@ -6,10 +6,10 @@
  * to invent new types of timeline tracks
  */
 
-#include "testApp.h"
+#include "ofApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
+void ofApp::setup(){
 	
 	ofBackground(255*.15);
 	ofSetFrameRate(30);
@@ -17,9 +17,6 @@ void testApp::setup(){
 	
 	ofEnableSmoothing();
 	ofEnableAlphaBlending();
-
-	//lets you use COMMAND+C and COMMAND+V actions on mac
-	ofxTimeline::removeCocoaMenusFromGlut("Empty Templates");
 	
 	timeline.setup();
 	timeline.setLoopType(OF_LOOP_NORMAL);
@@ -37,58 +34,58 @@ void testApp::setup(){
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
+void ofApp::update(){
 		
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
+void ofApp::draw(){
     float curtime = timeline.getCurrentTime();
 	timeline.draw();
 }
 
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key){
+void ofApp::keyPressed(int key){
 
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
+void ofApp::keyReleased(int key){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y ){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button){
 
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h){
 
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg){
 
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
 
 }
