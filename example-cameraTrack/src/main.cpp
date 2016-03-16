7fa75cc892d93497148763ea54ab0e9c9965c7c4
Elie Zananiri <prisonerjohn@gmail.com>
2016-03-16 10:06:51 -0400

    Updated main(), looks like everything still works on the programmable renderer.

--- example-cameraTrack/src/main.cpp
+++ example-cameraTrack/src/main.cpp
@@ -8,17 +8,15 @@
 
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
 
-//========================================================================
-int main( ){
+ //--------------------------------------------------------------
+int main()
+{
+	ofGLWindowSettings settings;
+	//settings.setGLVersion(4, 1);
+	settings.width = 1024;
+	settings.height = 768;
+	ofCreateWindow(settings);
 
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
-
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new ofApp());
-
+	ofRunApp(new ofApp());
 }
