2e83aab376f20f319f95fa1625dad56a4ee73698
Elie Zananiri <prisonerjohn@gmail.com>
2016-03-31 18:28:49 -0400

    Updated main()

--- example-timelineLib/src/main.cpp
+++ example-timelineLib/src/main.cpp
@@ -8,17 +8,15 @@
 
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofAppGlutWindow.h"
 
 //========================================================================
 int main( ){
 
-    ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 1024,768, OF_WINDOW);			// <-------- setup the GL context
+	ofGLWindowSettings settings;
+	settings.width = 1024;
+	settings.height = 768;
+	ofCreateWindow(settings);
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp( new ofApp());
+	ofRunApp(new ofApp());
 
 }
